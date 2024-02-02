14f5f863e772814fa3ccd3811aa204c39f553c89
LJJ <1483879133@qq.com>
2024-02-02 20:59:58 +0800

    新建后端项目

---
+++ backend/nexchange/src/main/java/org/nexchange/NexchangeApplication.java
@@ -0,0 +1,13 @@
+package org.nexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexchangeApplication.class, args);
+    }
+
+}
---
+++ backend/nexchange/src/test/java/org/nexchange/NexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
